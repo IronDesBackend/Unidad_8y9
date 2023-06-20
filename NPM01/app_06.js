4c6df11814b514bb1b1008af0cdc6346468bc93a
IronDesBackend <sebastian24gg@gmail.com>
2023-06-19 20:53:49 -0500

    add NPM01

---
+++ NPM01/app_06.js
@@ -0,0 +1,9 @@
+const colors = require('colors')
+
+console.log("Bon Día MInino, amiguiño".green.bgRed.bold)
+console.log("Hola Mundo JS, Node.js and NPM".yellow.bgCyan.italic)
+console.log("Hola Mundo JS, Node.js, NPM and Colors".yellow.bgCyan.inverse)
+
+//con "npm" y un par de comandos se hace muchas cosas
+//lo normal es usar el install, se borro el node_modules, para recuperarlo
+//y usar color, usa "npm install"
